48551a4ef1a709b19c3d02eae35e486082d99fd8
Xavier Duncan <xduncan@pm.me>
2018-12-02 15:41:06 -0500

    add labels to form input fields

--- src/form/form.js
+++ src/form/form.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 const Form = () =>
     <div>
-        <input type="text" name="city" placeHolder="Enter City..." />
-        <input type="text" name="country" placeHolder="Enter Country..." />
-        <input type="submit" value="submit" />
+        <form>
+            <label>
+                City:
+                <input
+                    type="text"
+                    name="city"
+                    placeholder="Enter City..." />
+            </label>
+            <label>
+                Country:
+                <input
+                    type="text"
+                    name="country"
+                    placeholder="Enter Country..." />
+            </label>
+            <input type="submit" value="submit" />
+        </form>
     </div>
 
 export default Form;
